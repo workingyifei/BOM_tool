e1a218d8372d786b4048a965d03e649ede22c720
Yifei Li <yifei.li@BYTON.COM>
2018-10-22 10:50:40 -0700

    syncs with MBOM

--- update SCBOM.py
+++ update SCBOM.py
@@ -1,4 +1,4 @@
-# The script below syncs the supply Chain BOM with Engineering BOM.
+# The script below syncs the supply Chain BOM with Manufacturing BOM.
 # Created by Yifei.li@byton.com
 
 import pandas as pd
@@ -7,24 +7,23 @@ import xlrd, openpyxl
 import csv, datetime
 import time
 
-
-#load CAD_BOM and SCBOM
-#load CAD_BOM and SCBOM
+#load MBOM and SCBOM
 def load():
-	# load Engineering BOM
-	# CAD_BOM = pd.read_csv(CAD_BOM_PATH)
-	CAD_BOM = pd.read_excel(CAD_BOM_PATH, sheet_name="BOM", skiprows=1)
-	CAD_BOM.columns = CAD_BOM.columns.str.replace("\(R\)\ ", "") # trim (R) away from the header
+	# load Manufacturing BOM
+	# MBOM = pd.read_csv(MBOM_PATH)
+	MBOM = pd.read_excel(MBOM_PATH, sheet_name="BOM")
+	MBOM.columns = MBOM.columns.str.replace("\(R\)\ ", "") # trim (R) away from the header
 
-	CAD_BOM.loc[CAD_BOM.System=="N Intelligent Car Experience ICE", "System"]= "N ICE"
+	# Rename N Intelligent Car Experience ICE
+	MBOM.loc[MBOM.System=="N Intelligent Car Experience ICE", "System"]= "N ICE"
 
 	# change Byton Part Number column to Byton PN
-	CAD_BOM = CAD_BOM.rename(index=str, columns={"Byton Part number": "Byton PN"})
+	MBOM = MBOM.rename(index=str, columns={"Title": "Byton PN", })
 
 	# change identifier type to string
-	CAD_BOM["Identifier"] = CAD_BOM["Identifier"].apply(str)
+	MBOM["Identifier"] = MBOM["Identifier"].apply(str)
 
-	CAD_BOM = CAD_BOM.reset_index(drop=True)
+	MBOM = MBOM.reset_index(drop=True)
 
 	#     # load Supply Chain BOM from MULTIPLE tabs in an Excel
 	#     df= pd.read_excel(SCBOM_PATH, sheet_name=system_name)
@@ -39,15 +38,18 @@ def load():
 	# pd.to_excel doesn't allow column name greater than 31 characters
 	SCBOM.loc[SCBOM.System=="N Intelligent Car Experience ICE", "System"]= "N ICE"
 
+	# change Byton Part Number column to Byton PN
+	SCBOM = SCBOM.rename(index=str, columns={"Title": "Byton PN", })
+
 	# reset index of SCBOM
 	SCBOM.reset_index(drop=True, inplace=True)
 	print("\nCompleted loading excel files...\n")
-	return CAD_BOM, SCBOM
+	return MBOM, SCBOM
 
-# search PN and/or Rev in SCBOM
+# search PN and/or Rev in updated SCBOM
 def search(df, PN, Rev):
 	# df = df.loc[(df["Title"]==PN) & (df["Revision"]==Rev)]
-	# CHANGE THIS IF CAD_BOM COLUMN NAME CHANGED!!!
+	# CHANGE THIS IF MBOM COLUMN NAME CHANGED!!!
 	df = df.loc[(df["Byton PN"]==PN)]	# search PN only 
 	index = df.index.values
 
@@ -62,13 +64,13 @@ def search(df, PN, Rev):
 
 	# no entry found
 	else:
-	    return None
+		return None
         
 # copy SCBOM info and paste into SCBOM_updated
 def copy_and_paste_row(df1, index1, df2, index2):  # index1 and index2 are int
 	# copy df2 info into df1
-	# only copy columns that are not on CAD_BOM
-	df1.loc[index1, df1.columns.tolist()[17:81]] = df2.loc[index2, df2.columns.tolist()[14:78]]
+	# only copy columns that are not on MBOM
+	df1.loc[index1, df1.columns.tolist()[SCBOM_updated_columns_start:SCBOM_updated_columns_end]] = df2.loc[index2, df2.columns.tolist()[SCBOM_columns_start:SCBOM_columns_end]]
 	return df1
 
 # save dataframe to Excel
@@ -95,15 +97,15 @@ def save(df):
 
 #     df.to_csv("Supply Chain BOM.csv",quoting=csv.QUOTE_NONE, escapechar="\\")
 
-def main(CAD_BOM, SCBOM, SCBOM_updated):
+def main(MBOM, SCBOM, SCBOM_updated):
 
 	SCBOM_columns = SCBOM.columns.tolist()
 	SCBOM_columns_size = len(SCBOM_columns)
-	CAD_BOM_columns = CAD_BOM.columns.tolist()
-	CAD_BOM_columns_size = len(CAD_BOM_columns)
+	MBOM_columns = MBOM.columns.tolist()
+	MBOM_columns_size = len(MBOM_columns)
 
 	print("Before Sync")
-	print("CAD_BOM shape: ", CAD_BOM.shape)
+	print("MBOM shape: ", MBOM.shape)
 	print("SCBOM shape: ", SCBOM.shape)
 	print("SCBOM_updated: {}\n".format(SCBOM_updated.shape))
 
@@ -117,7 +119,7 @@ def main(CAD_BOM, SCBOM, SCBOM_updated):
 	# loop through SCBOM 
 	for index, row in SCBOM.iterrows():
 		print("\rUpdating row # {0} of Supply Chain BOM. Progress: {1}%".format(index, round(index*100/SCBOM.shape[0]), 4), end="", flush=True)
-		PN = row["Title"]
+		PN = row["Byton PN"]
 		Rev = row["Revision"]
 		# same part could be structured differently but the part is the same, 
 		# so if same part found at multiple places, we will just pick one
@@ -135,7 +137,7 @@ def main(CAD_BOM, SCBOM, SCBOM_updated):
 			SCBOM.loc[index, ["Last Modified Date"]] = datetime.date.today()
 			# SCBOM.loc[index, ["Last Modified Date"]] = datetime.date(2019, 9, 28)
 			columns_to_copy = SCBOM.columns.tolist()[SCBOM_columns_start:SCBOM_columns_end]
-			other_columns = ["Title", "Revision", "Description", "System", "SubSystem", "Part Type"]
+			other_columns = ["Byton PN", "Revision", "Description", "System", "SubSystem", "Part Type"]
 			columns_to_copy = columns_to_copy + other_columns
 			SCBOM_updated = SCBOM_updated.append(SCBOM.loc[index, columns_to_copy], ignore_index=True)
 
@@ -164,80 +166,74 @@ def main(CAD_BOM, SCBOM, SCBOM_updated):
 	save(SCBOM_updated)
 
 	print("After Sync")
-	print("CAD_BOM shape: ", CAD_BOM.shape)
+	print("MBOM shape: ", MBOM.shape)
 	print("SCBOM shape: ", SCBOM.shape)
 	print("SCBOM_updated ", SCBOM_updated.shape)
 
 
-	print("\n# of new parts added: ", CAD_BOM.shape[0]-same_parts)
+	print("\n# of new parts added: ", MBOM.shape[0]-same_parts)
 	print("# of old parts removed: ", removed_parts_count)
-	print("# of additional columns added in CAD_BOM: ", SCBOM_updated.shape[1] - SCBOM.shape[1])
+	print("# of additional columns added in MBOM: ", SCBOM_updated.shape[1] - SCBOM.shape[1])
 
 
 if __name__ == "__main__":
 	start_time = time.time()
 
-	# put up-to-date CAD_BOM and SCBOM here for sync
-	CAD_BOM_PATH = './Data/AAZZ000001NN03_VP_BoM_10-15-18.xlsx'
-	SCBOM_PATH = './Data/Copy of Supply Chain BOM_2018-10-03_final.xlsx'
+	# put up-to-date MBOM and SCBOM here for sync
+	MBOM_PATH = './Data/AAZZ000001NN03_VP_BoM_10-8-18 at 11.10 AM_MFG_Release_v1.0.xlsx'
+	SCBOM_PATH = './Data/Supply Chain BOM_2018-10-03_final.xlsx'
 
-	# needs to change the followings based on how the CAD_BOM and SCBOM columns are structured 
+	MBOM, SCBOM = load()
+
+	# indexes of the columns we are copying from and pasting into
 	# 0 index
-	SCBOM_columns_start = 14 # SC columns start at column # 15
-	SCBOM_columns_end = 78 # SC columns end at column # 77
-	SCBOM_updated_columns_start = 14 
-	SCBOM_updated_columns_end = 78
+	SCBOM_columns_start = 14 # SC columns start at column index 14, "Engineer"
+	SCBOM_columns_end = SCBOM.shape[1] # SC columns end at column index 77
+	SCBOM_updated_columns_start = MBOM.shape[1] # 21 columns
+	SCBOM_updated_columns_end = SCBOM_updated_columns_start + (SCBOM_columns_end - SCBOM_columns_start) + 1 # 21 + 77-14+1 = 85
 
 
-	# tab names in supply chain BOM.xlsx and CAD_BOM.xlsx
+	# tab names in supply chain BOM.xlsx and MBOM.xlsx
 	system_name = ["A BIW", "B Closures", "C Exterior", "D Interior", "E Chassis", "F Thermal Management", "G Drivetrain",
 				   "H Power Electronics", "J HV Battery", "K Autonomy", "L Low Voltage Systems", "M Connectivity", 
 	          	   "N ICE", "X Raw Materials", "Y Fasteners", "Z Vehicle Top Level Cfg"]
 
+	
 
-	CAD_BOM, SCBOM = load()
+	# # create Parent and Level column in MBOM
+	# MBOM.insert(0, column="Level", value="")
+	# MBOM.insert(1, column="Parent", value="")
 
-	# create Parent and Level column in CAD_BOM
-	CAD_BOM.insert(0, column="Level", value="")
-	CAD_BOM.insert(1, column="Parent", value="")
+	# for index, row in MBOM.iterrows():
+	# 	identifier = str(row["Identifier"]) 
+	# 	level = identifier.count("|")
+	# 	MBOM.loc[index, ["Level"]] = level
 
-	for index, row in CAD_BOM.iterrows():
-		identifier = str(row["Identifier"]) 
-		level = identifier.count("|")
-		CAD_BOM.loc[index, ["Level"]] = level
+	# 	# find out PN of part's parent
+	# 	identifier_parent = identifier[0:-2]
+	# 	if identifier_parent == '':
+	# 		identifier_parent = ''
+	# 	elif identifier_parent[-1]=='|':
+	# 		identifier_parent = identifier_parent[0:-1]
+	# 	else:
+	# 		identifier_parent = identifier_parent
 
-		# find out PN of part's parent
-		identifier_parent = identifier[0:-2]
-		if identifier_parent == '':
-			identifier_parent = ''
-		elif identifier_parent[-1]=='|':
-			identifier_parent = identifier_parent[0:-1]
-		else:
-			identifier_parent = identifier_parent
+	# 	PN_parent = MBOM[MBOM["Identifier"]==identifier_parent]["Byton PN"]
+	# 	if PN_parent.empty == True:
+	# 	    PN_parent = ""
+	# 	else:
+	# 	    PN_parent = PN_parent.values
+	# 	MBOM.loc[index, ["Parent"]] = PN_parent
+	# print("Parent PN and Level are generated in CAD BOM...\n")
 
-		PN_parent = CAD_BOM[CAD_BOM["Identifier"]==identifier_parent]["Byton PN"]
-		if PN_parent.empty == True:
-		    PN_parent = ""
-		else:
-		    PN_parent = PN_parent.values
-		CAD_BOM.loc[index, ["Parent"]] = PN_parent
-	print("Parent PN and Level are generated in CAD BOM...\n")
-
-	# create a updated SCBOM with columns from SCBOM and data from CAD_BOM
-	SCBOM_updated = CAD_BOM.copy()
-	# copy only colums that do not exist in CAD_BOM
+	# create a updated SCBOM with columns from SCBOM and data from MBOM
+	SCBOM_updated = MBOM.copy()
+	# copy only colums that do not exist in MBOM
 	for each in SCBOM.columns.tolist()[SCBOM_columns_start:]:
 		SCBOM_updated[each] = ""
 
 
-	# needs to change the followings based on how the CAD_BOM and SCBOM columns are structured 
-	# 0 index
-	SCBOM_columns_start = 14 # Supply Chain info starts at column # 14 in SCBOM
-	SCBOM_columns_end = SCBOM.shape[1] 
-	SCBOM_updated_columns_start = CAD_BOM.shape[1]
-	SCBOM_updated_columns_end = SCBOM.shape[1] + SCBOM_updated_columns_start-SCBOM_columns_start
-
-	main(CAD_BOM, SCBOM, SCBOM_updated)
+	main(MBOM, SCBOM, SCBOM_updated)
 
 	execution_time = round(time.time() - start_time, 2)
 	print("\nThis script took--- {} seconds ---".format(execution_time))
